049b7cdb2d41e55ef58e354316464ad03e60fe45
Frank Lan <frank.lan@balanceinternet.com.au>
2019-08-29 16:53:50 +1000

    add jobs table migration and seeder

---
+++ backend/database/migrations/2019_08_29_045848_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //job title,job description,date,location,applicants
+        Schema::create('jobs', function (Blueprint $table): void {
+            $table->increments('id');
+            $table->text('job title');
+            $table->text('job description');
+            $table->date('date');
+            $table->text('location');
+            $table->text('applicants');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
