c64b6faeb36d29ca8fef350db4effa3db84a327f
Frank Lan <frank.lan@balanceinternet.com.au>
2019-08-30 09:44:59 +1000

    add data model of Job

---
+++ backend/app/Job.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Job.
+ *
+ * @author Frank Lan <franklan118@gmail.com>
+ */
+class Job extends Model
+{
+}
