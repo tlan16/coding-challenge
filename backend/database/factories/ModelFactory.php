7624f8636478702df53c2a4aac66ab60c260929c
Frank Lan <frank.lan@balanceinternet.com.au>
2019-08-30 13:20:15 +1000

    add factories for job

--- backend/database/factories/ModelFactory.php
+++ backend/database/factories/ModelFactory.php
@@ -17,3 +17,49 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
     ];
 });
+
+$factory->define(App\Job::class, function (Faker\Generator $faker): array {
+    $numberOfApplicants = $faker->numberBetween(0, 10);
+    $applicants = array_map(
+        function() use ($faker) :string {
+            return $faker->name;
+        },
+        range(1, $numberOfApplicants)
+    );
+
+    return [
+        'job title' => $faker->jobTitle,
+        'job description' => $faker->sentence,
+        'date' => $faker->date(),
+        'location' => $faker->city,
+        'applicants' => implode(',', $applicants),
+    ];
+});
+
+$factory->defineAs(App\Job::class, 'no applicant', function (Faker\Generator $faker): array {
+    return [
+        'job title' => $faker->jobTitle,
+        'job description' => $faker->sentence,
+        'date' => $faker->date(),
+        'location' => $faker->city,
+        'applicants' => '',
+    ];
+});
+
+$factory->defineAs(App\Job::class, 'one or more applicant', function (Faker\Generator $faker): array {
+    $numberOfApplicants = $faker->numberBetween(1, 10);
+    $applicants = array_map(
+        function() use ($faker) :string {
+            return $faker->name;
+        },
+        range(1, $numberOfApplicants)
+    );
+
+    return [
+        'job title' => $faker->jobTitle,
+        'job description' => $faker->sentence,
+        'date' => $faker->date(),
+        'location' => $faker->city,
+        'applicants' => implode(',', $applicants),
+    ];
+});
